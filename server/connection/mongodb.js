aab7e13e09379402fc0cacb263a38daba7701819
TJ DIBBS <oderindejames02@gmail.com>
2022-01-10 05:52:12 +0100

    []

--- server/connection/mongodb.js
+++ server/connection/mongodb.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-	process.env.MONGO_URI || `mongodb://localhost:27017/zablot`,
+	process.env.MONGO_URI,
 	{
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
