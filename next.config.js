df76ea2a669749eb20bd912f0a3a6a221112cb16
tj_dibbs <oderindejames02@gmail.com>
2022-10-18 02:08:13 +0100

    Tailwind not working in production resolve

--- next.config.js
+++ next.config.js
@@ -1,21 +1,13 @@
-const { withPlausibleProxy } = require("next-plausible");
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    IMAGE_LOADER: "custom",
+module.exports = {
+  reactStrictMode: false,
+  eslint: { ignoreDuringBuilds: true },
+  async redirects() {
+    return [
+      {
+        source: "/dashboard/:path",
+        destination: "/dashboard?p=:path",
+        permanent: true,
+      },
+    ];
   },
-  images: {
-    loader: "custom",
-    domains: [
-      "localhost:3000",
-      "zablot2.herokuapp.com",
-      "play.tailwindcss.com",
-    ],
-  },
-  reactStrictMode: true,
-  poweredByHeader: false,
-  output: "standalone",
 };
-
-module.exports = withPlausibleProxy()(nextConfig);
